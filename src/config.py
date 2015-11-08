7b4186bec061ef4796ed808dca5e7f66b9146ad5
Daniel Clarke <u8sand@gmail.com>
2015-11-08 14:42:48 -0500

    Added .mka as audio filetype

--- src/config.py
+++ src/config.py
@@ -10,10 +10,10 @@ class Config:
   remoteListen = '0.0.0.0:8474'
   onlineHelpUrl = 'http://mochi-player.github.io/help.html'
   monospaceFont = 'Courier New'
-  audioFiletypes = ["*.mp3","*.ogg","*.wav","*.wma","*.m4a","*.aac","*.ac3","*.ape","*.flac","*.ra"]
-  videoFiletypes = ["*.avi","*.divx","*.mpg","*.mpeg","*.m1v","*.m2v","*.mpv","*.dv","*.3gp","*.mov","*.mp4","*.m4v","*.mqv","*.dat","*.vcd","*.ogm","*.ogv","*.asf","*.wmv","*.vob","*.mkv","*.ram","*.flv","*.rm","*.ts","*.rmvb","*.dvr-ms","*.m2t","*.m2ts","*.rec","*.f4v","*.hdmov","*.webm","*.vp8","*.letv","*.hlv"]
+  audioFiletypes = ['*.mp3','*.ogg','*.wav','*.wma','*.m4a','*.aac','*.ac3','*.ape','*.flac','*.ra','*.mka']
+  videoFiletypes = ['*.avi','*.divx','*.mpg','*.mpeg','*.m1v','*.m2v','*.mpv','*.dv','*.3gp','*.mov','*.mp4','*.m4v','*.mqv','*.dat','*.vcd','*.ogm','*.ogv','*.asf','*.wmv','*.vob','*.mkv','*.ram','*.flv','*.rm','*.ts','*.rmvb','*.dvr-ms','*.m2t','*.m2ts','*.rec','*.f4v','*.hdmov','*.webm','*.vp8','*.letv','*.hlv']
   mediaFiletypes = audioFiletypes + videoFiletypes
-  subtitleFileypes = ["*.sub","*.srt","*.ass","*.ssa"]
+  subtitleFileypes = ['*.sub','*.srt','*.ass','*.ssa']
   help = False
   verbose = False
   script = True # are we a python script, or a wrapped executable?
